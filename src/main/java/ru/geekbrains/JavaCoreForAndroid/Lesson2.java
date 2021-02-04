ea7bc6f6560dc24fbf0f41b4514073fb676adf3b
StDimensiy <stdimensiy@yandex.ru>
2021-02-05 00:01:18 +0300

    Задача №1 - выполнена.

--- src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson2.java
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson2.java
@@ -6,10 +6,51 @@ package ru.geekbrains.JavaCoreForAndroid;
  *
  * @Author Student Dmitry Veremeenko aka StDimensiy
  * Group 24.12.2020
- *
+ * <p>
  * HomeWork for lesson2
  * Created 05.02.2021
  * v2.0
  */
 public class Lesson2 {
+    public static void main(String[] args) {
+        // Переменные
+        int[] originIntArray = {0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0};
+
+        //Задание №1 (второй способ решения задачи закомментирован)
+        System.out.println("Задание №1 (1 строка оригинальный массив, 2 строка инвертированный массив (для наглядности)");
+        printArrLine(originIntArray);
+        inversionArray(originIntArray); // обращение к методу как к НЕвозвращающему значение (как к процедуре)
+        printArrLine(originIntArray);
+        // printArrLine(inversionArray(originIntArray)); второй способ получить результат обращаясь к методу как к возвращающему значение
+        System.out.println("            *** Массив обработан методом inversionArray() - задача выполнена.");
+        System.out.println();
+
+    }
+
+    // Дополнительный метод - выводит элементы массива тип int в форматированную строку (для удобства )
+    // Самоподготовка (СамПо) вне занятия
+    public static void printArrLine(int[] a) {
+        for (int elem : a) {
+            System.out.print(elem + " ");
+        }
+        System.out.println();
+    }
+
+    // Дополнительный перегруженный метод - выводит элементы массива строк в форматированную строку (для удобства)
+    // Самоподготовка (СамПо) вне занятия
+    public static void printArrLine(String[] a) {
+        for (String elem : a) {
+            System.out.print(elem + " ");
+        }
+        System.out.println();
+    }
+
+    // Задание №1 - метод инвертирует элементы массива 1 в 0 и наоборот (ели не 0, тогда тоже переведет в 0)
+    public static int[] inversionArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] == 1 ? 0 : 1;
+        }
+        return arr;
+    }
+
 }
